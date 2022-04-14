7b0afa57e9fc442fd14c6c2a405192584e9a2798
wook <gus5427@naver.com>
2022-04-14 18:15:30 +0900

    Docs : add static files

---
+++ pages/story/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NextPage} from "next";
+import BottomNavigation from "../../components/common/BottomNavigation";
+
+
+const Story:NextPage= () => {
+    return (
+        <div>
+            <p>사연페이지</p>
+            <BottomNavigation/>
+        </div>
+    );
+};
+
+export default Story;
